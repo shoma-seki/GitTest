1d8d86d30b2e8ebecdca102e38930346847ac90b
shoma-seki <146906404+shoma-seki@users.noreply.github.com>
2023-10-11 11:50:41 +0900

    Hello GIthubに変更

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		Novice::ScreenPrintf(0, 0, "Hello World");
+		Novice::ScreenPrintf(0, 0, "Hello Github");
 		///
 		/// ↑描画処理ここまで
 		///
